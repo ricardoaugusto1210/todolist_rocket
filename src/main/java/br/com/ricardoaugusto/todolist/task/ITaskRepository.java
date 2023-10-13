a0bcea3951c3d234948d8ce2cd3c3807e07bbb2a
ricardoaugusto1210 <ricardoaugusto.eb@gmail.com>
2023-10-13 14:36:53 -0300

    Dockerfile deploy

---
+++ src/main/java/br/com/ricardoaugusto/todolist/task/ITaskRepository.java
@@ -0,0 +1,14 @@
+package br.com.ricardoaugusto.todolist.task;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID> {
+
+    List<TaskModel> findByIdUser(UUID uuid);
+
+    TaskModel findByIdAndIdUser(UUID id, UUID idUser);
+}
